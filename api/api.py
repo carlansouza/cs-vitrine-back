bad62b36ef06bc11437df5840d197e9d1cdb65d4
carlansouza <carlanogueirasouza@gmail.com>
2024-06-04 20:47:22 -0300

    configuration vercel

--- api/api.py
+++ api/api.py
@@ -12,6 +12,7 @@ app = FastAPI(
 )
 
 origins = [
+    "https://cs-vitrine-back-445greuix-carlas-projects-420903b3.vercel.app"
     "http://localhost",
     "http://localhost:8000",
     "http://localhost:4200"
