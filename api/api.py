4f2454ee93de51c449762ac88f4cf6530c04362f
carlansouza <carlanogueirasouza@gmail.com>
2024-06-04 19:28:22 -0300

    ALTER VERCEL

---
+++ api/api.py
@@ -0,0 +1,35 @@
+from fastapi import  FastAPI
+from src.modules.user.router import router as user_router
+from src.modules.auth.router import router as auth_router
+from src.modules.car.router import router as car_router
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="CSCar Backend API",
+    description="CSCar Project API documentation",
+    version="0.0.1",
+    debug=True,
+)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:4200"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],  # Métodos permitidos
+    allow_headers=["*"]
+)
+
+app.include_router(user_router)
+app.include_router(auth_router)
+app.include_router(car_router)
+
+@app.get("/ping")
+async def pong():
+    return {"ping": "pong!"}
+
--- main.py
+++ main.py
@@ -1,43 +1,12 @@
-from fastapi import  FastAPI
-from src.modules.user.router import router as user_router
-from src.modules.auth.router import router as auth_router
-from src.modules.car.router import router as car_router
-from fastapi.middleware.cors import CORSMiddleware
-
+from dotenv import load_dotenv
 import uvicorn
 import os
+from api.api import app
 
+load_dotenv()   
 
-port = os.environ.get('PORT') or 8000;
-
-app = FastAPI(
-    title="CSCar Backend API",
-    description="CSCar Project API documentation",
-    version="0.0.1",
-    debug=True,
-)
-
-origins = [
-    "http://localhost",
-    "http://localhost:8000",
-    "http://localhost:4200"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],  # Métodos permitidos
-    allow_headers=["*"]
-)
-
-app.include_router(user_router)
-app.include_router(auth_router)
-app.include_router(car_router)
-
-@app.get("/ping")
-async def pong():
-    return {"ping": "pong!"}
+PORT = int(os.getenv('PORT', 8000))
+HOST = '0.0.0.0' 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run('app.api: app', host=HOST, port=PORT, reload=True)
